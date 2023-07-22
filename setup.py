06b580ad9cc92f641e372c358b907c8265cf6a9e
Ran-x2 <112669569+Ran-x2@users.noreply.github.com>
2023-07-22 03:41:37 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="TransCompR",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[i.strip() for i in open("requirements.txt").readlines()],
     #package_data={
