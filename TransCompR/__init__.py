de9428476aab3834c08a83adda847afbe3c30528
Brubaker Lab <77795297+BrubakerLab@users.noreply.github.com>
2023-08-20 19:19:58 -0400

    Delete TransCompR directory

--- TransCompR/__init__.py
+++
@@ -1,4 +0,0 @@
-from .TransCompR import TransCompR
-from .tools import  highly_variable_features, gsea
-from .utils import  indices, validate_dataframes, validate_hvf_and_pcs, validate_classes
-from .plot import loadings, var_explained, gsea_bar, partition, covariate_pc_interaction
